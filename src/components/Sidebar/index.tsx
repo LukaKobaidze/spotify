54802b137a1f4dd574fd56d9abdd0a30419eb1f7
Luka <lukakobaidze2006@gmail.com>
2023-08-14 14:26:35 +0400

    update

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,17 +1,19 @@
-import Link from "next/link";
-import styles from './Sidebar.module.scss'
+import Link from 'next/link';
+import styles from './Sidebar.module.scss';
 
-interface Props {
-  
-}
+interface Props {}
 
 export default function Sidebar(props: Props) {
-
-  return <aside className={styles.sidebar}>
-
-    <nav className={`roundedContainer ${styles.nav}`}>
-      <Link href="/" className="textButton">Home</Link>
-      <Link href="/search" className="textButton">Search</Link>
-    </nav>
-  </aside>
+  return (
+    <aside className={styles.sidebar}>
+      <nav className={`roundedContainer ${styles.nav}`}>
+        <Link href="/" className="textButton">
+          Home
+        </Link>
+        <Link href="/search" className="textButton">
+          Search
+        </Link>
+      </nav>
+    </aside>
+  );
 }
